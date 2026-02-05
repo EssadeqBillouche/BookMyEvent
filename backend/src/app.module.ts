50229fbbc44f8f157bce21c7b787ad89a096be77
essadeq billouche <essadeq.billouche@gmail.com>
2026-02-05 17:23:50 +0100

    feat: Add EventModule and RegistrationModule to AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { EventModule } from './event/event.module';
+import { RegistrationModule } from './registration/registration.module';
 import { typeOrmConfig } from 'config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,6 +13,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [ 
             UserModule,
             AuthModule,
+            EventModule,
+            RegistrationModule,
             ConfigModule.forRoot({isGlobal : true}),
             TypeOrmModule.forRootAsync(typeOrmConfig)
           ],
